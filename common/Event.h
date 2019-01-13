06f446061c5ae9607631c68e27a62365861e2992
mmwebster <milo.webster@gmail.com>
2019-01-12 18:53:19 -0800

    Added subsys-common dependencies from previous work

---
+++ common/Event.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <array>
+#include <vector>
+
+#include "Gpio.h"
+
+class Event {
+ public:
+  // Event types
+  enum Type { kNone, kCanRx, kTimerTimeout, kAdcConversion,
+    kDigInTransition, kUartRx };
+
+  Event(Type t, Gpio adcPin, uint32_t adcValue);
+  Event(Type t, uint32_t canEid, std::array<uint16_t, 8> canFrame);
+  Event(Type t, DigitalInput pin, bool currentState);
+  Event(Type t, char byte);
+  Event();
+
+  Type type();
+
+  // type-specific member functions (see note in source)
+  Gpio adcPin();
+  uint32_t adcValue();
+  uint32_t canEid();
+  std::array<uint16_t, 8> canFrame();
+  DigitalInput digInPin();
+  bool digInState();
+  char getByte();
+
+ private:
+  Type m_type = kNone;
+
+  /**
+   * @note Allocation of 320 bytes (10 32 bit integers), of course,
+   *       overflows a 128 byte static thread workspace and breaks
+   *       the kernel. Be careful with static thread workspace
+   */
+  std::array<uint16_t, 10> m_params;
+};
---
+++ common/EventQueue.cpp
@@ -0,0 +1,52 @@
+#include "EventQueue.h"
+
+#include <stdint.h>
+
+#include <mutex>
+
+#include "hal.h"
+
+EventQueue::EventQueue() {}
+
+Event EventQueue::pop() {
+  // acquire lock guard in current scope
+  std::lock_guard<chibios_rt::Mutex> lock(m_queueMut);
+  // pop item from queue and return
+  return m_queue.PopFront();
+}
+
+bool EventQueue::tryPush(Event e) {
+  // acquire lock in current scope
+  bool didAcquire = m_queueMut.tryLock();
+  //std::unique_lock<chibios_rt::Mutex> queueLock(m_queueMut,
+  //    std::try_to_lock);
+  if (!didAcquire) {
+    // don't push, indicate failure
+    return false;
+  } else {
+    // push item, indicate success
+    m_queue.PushBack(e);
+    m_queueMut.unlock();
+    return true;
+  }
+}
+
+void EventQueue::push(Event e) {
+  // acquire lock in current scope
+  std::lock_guard<chibios_rt::Mutex> queueGuard(m_queueMut);
+  // push item
+  m_queue.PushBack(e);
+}
+
+void EventQueue::push(std::vector<Event> events) {
+  // acquire lock in current scope
+  std::lock_guard<chibios_rt::Mutex> queueGuard(m_queueMut);
+  // push items
+  for (Event e : events) {
+    m_queue.PushBack(e);
+  }
+}
+
+bool EventQueue::wait() { return false; }
+
+size_t EventQueue::size() { return m_queue.Size(); }
